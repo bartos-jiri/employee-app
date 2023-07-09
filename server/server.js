9e550d0ac6942c3077731cfb3dbe5d67b4f9e827
Jiří Bartoš <bartosjir@gmail.com>
2023-07-09 08:02:17 +0200

    S1 - Allow server to generate mock data

--- server/server.js
+++ server/server.js
@@ -1,4 +1,30 @@
+const { faker } = require('@faker-js/faker');
+const fs = require("fs");
 const jsonServer = require("json-server");
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+const generateData = (count) => {
+  const data = [];
+
+  for(let i = 1; i <= count; i++) {
+    data.push({
+      id: i,
+      name: faker.person.fullName(),
+      jobTitle: faker.person.jobType(),
+      tenure: faker.number.int(20).toString(),
+      gender: capitalize(faker.person.sex())
+    })
+  }
+
+  return data;
+}
+
+fs.writeFileSync(
+  "./db.json",
+  JSON.stringify({ employees: generateData(parseInt(process.env.DATA_COUNT || '20', 10)) })
+);
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
