19d2641e5cdeef83acd3f1fd91f0b6608c385699
Jiří Bartoš <bartosjir@gmail.com>
2023-07-09 10:18:54 +0200

    C1 - Adjusted page title

--- client/src/core/LoadingPage.tsx
+++ client/src/core/LoadingPage.tsx
@@ -1,11 +1,9 @@
 import Spinner from "react-bootstrap/Spinner";
 
-export const LoadingPage: React.FC = () => {
-  return (
-    <div className="w-100 flex-fill d-flex justify-content-center align-items-center">
-      <Spinner animation="border" role="status" variant="primary">
-        <span className="visually-hidden opacity-0">Loading...</span>
-      </Spinner>
-    </div>
-  );
-};
+export const LoadingPage: React.FC = () => (
+  <div className="w-100 flex-fill d-flex justify-content-center align-items-center">
+    <Spinner animation="border" role="status" variant="primary">
+      <span className="visually-hidden opacity-0">Loading...</span>
+    </Spinner>
+  </div>
+);
