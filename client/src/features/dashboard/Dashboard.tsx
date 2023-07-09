6a9c8345d7e28e0edc5450580877bc8a7b47e223
Jiří Bartoš <bartosjir@gmail.com>
2023-07-09 16:40:12 +0200

    C5 - Added empty view for Dashboard

--- client/src/features/dashboard/Dashboard.tsx
+++ client/src/features/dashboard/Dashboard.tsx
@@ -2,14 +2,25 @@ import { EmployeeTable } from "./tables/EmployeeTable";
 import { EmployeeGraphs } from "./graphs/EmployeeGraphs";
 
 import classes from "./Dashboard.module.scss";
+import { useLoaderData } from "react-router-dom";
+import { Employee } from "../../models/Employee";
+import { EmptyDashboard } from "./EmptyDashboard";
 
-export const Dashboard: React.FC = () => (
-  <div>
-    <div className={`${classes.top} mb-2 d-flex`}>
-      <EmployeeTable />
-    </div>
+export const Dashboard: React.FC = () => {
+  const employees = useLoaderData() as Employee[];
+
+  if (!employees.length) {
+    return <EmptyDashboard />;
+  }
+
+  return (
     <div>
-      <EmployeeGraphs />
+      <div className={`${classes.top} mb-2 d-flex`}>
+        <EmployeeTable />
+      </div>
+      <div>
+        <EmployeeGraphs />
+      </div>
     </div>
-  </div>
-);
+  );
+};
---
+++ client/src/features/dashboard/EmptyDashboard.tsx
@@ -0,0 +1,5 @@
+export const EmptyDashboard: React.FC = () => (
+  <div className="flex-fill d-flex justify-content-center align-items-center">
+    No data to show. Add an employee.
+  </div>
+);
