8331044afc0b2ee40ec9b877075adf3d824fee24
Jiří Bartoš <bartosjir@gmail.com>
2023-07-09 16:51:08 +0200

    C4 - Added submitting state to NewEmployeeForm

--- client/src/features/new-employee/NewEmployeeForm.tsx
+++ client/src/features/new-employee/NewEmployeeForm.tsx
@@ -1,56 +1,68 @@
 import F from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { Form } from "react-router-dom";
+import { Form, useNavigation } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 
-export const NewEmployeeForm: React.FC = () => (
-  <div className="d-flex flex-column align-items-center">
-    <Form method="post">
-      <h1 className="mb-4">Add New Employee</h1>
+export const NewEmployeeForm: React.FC = () => {
+  const navigation = useNavigation();
 
-      <F.Group className="mb-3" controlId="name">
-        <F.Label>Name</F.Label>
-        <F.Control name="name" placeholder="John Doe" required />
-      </F.Group>
+  const submitting = navigation.state === "submitting";
 
-      <F.Group className="mb-3" controlId="jobTitle">
-        <F.Label>Job Title</F.Label>
-        <F.Control name="jobTitle" placeholder="Developer" required />
-      </F.Group>
+  return (
+    <div className="d-flex flex-column align-items-center">
+      <Form method="post">
+        <h1 className="mb-4">Add New Employee</h1>
 
-      <F.Group className="mb-3" controlId="tenure">
-        <F.Label>Tenure</F.Label>
-        <F.Control
-          name="tenure"
-          type="number"
-          placeholder="0"
-          required
-          min="0"
-        />
-      </F.Group>
+        <F.Group className="mb-3" controlId="name">
+          <F.Label>Name</F.Label>
+          <F.Control name="name" placeholder="John Doe" required />
+        </F.Group>
 
-      <F.Group className="mb-4" controlId="gender">
-        <F.Label>Gender</F.Label>
-        <F.Check
-          type="radio"
-          id="gender-male"
-          name="gender"
-          value="Male"
-          label="Male"
-          required
-        />
-        <F.Check
-          type="radio"
-          id="gender-female"
-          name="gender"
-          value="Female"
-          label="Female"
-          required
-        />
-      </F.Group>
+        <F.Group className="mb-3" controlId="jobTitle">
+          <F.Label>Job Title</F.Label>
+          <F.Control name="jobTitle" placeholder="Developer" required />
+        </F.Group>
 
-      <Button className="mx-auto" type="submit">
-        Submit
-      </Button>
-    </Form>
-  </div>
-);
+        <F.Group className="mb-3" controlId="tenure">
+          <F.Label>Tenure</F.Label>
+          <F.Control
+            name="tenure"
+            type="number"
+            placeholder="0"
+            required
+            min="0"
+          />
+        </F.Group>
+
+        <F.Group className="mb-4" controlId="gender">
+          <F.Label>Gender</F.Label>
+          <F.Check
+            type="radio"
+            id="gender-male"
+            name="gender"
+            value="Male"
+            label="Male"
+            required
+          />
+          <F.Check
+            type="radio"
+            id="gender-female"
+            name="gender"
+            value="Female"
+            label="Female"
+            required
+          />
+        </F.Group>
+
+        <div className="d-flex align-items-center">
+          <Button type="submit" disabled={submitting}>
+            Submit
+          </Button>
+          {submitting && (
+            <Spinner className="ms-2" size="sm" variant="primary" />
+          )}
+        </div>
+      </Form>
+    </div>
+  );
+};
